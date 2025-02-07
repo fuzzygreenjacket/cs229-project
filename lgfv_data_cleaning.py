b14fc2f534c33ac3ffcd15f57126f56a74257267
fuzzygreenjacket <matthewlhsu@gmail.com>
2025-02-06 20:55:15 -0800

    filtered merged lgfv data by date
    filtered merged lgfv data by date (listing date earlier than 2024-12-31 and delisting date later than 2018-01-01)

--- lgfv_data_cleaning.py
+++ lgfv_data_cleaning.py
@@ -8,3 +8,7 @@ df_4 = pd.read_excel("LGFV DEBT RISK/LGFV Bond Full list/城投债大全(2025-02
 new_df = pd.concat([df_1, df_2, df_3, df_4])
 
 new_df.to_excel("LGFV_merged.xlsx")
+
+df_merged = pd.read_excel("LGFV_merged.xlsx")
+df_merged_filtered = df_merged[(df_merged["Listing Date"].notna()) & (df_merged["Delisting Date"].notna()) & (df_merged["Listing Date"] <= "2024-12-31") & (df_merged["Delisting Date"] >= "2018-01-01")].copy()
+df_merged_filtered.to_excel("LGFV_merged_2018_to_2024.xlsx")
