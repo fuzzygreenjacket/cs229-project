bf3998c15419a6ea644625e5dde7ac65eea3f4f0
fuzzygreenjacket <matthewlhsu@gmail.com>
2025-02-19 17:26:09 -0800

    modified baseline linear regression
    modified baseline linear regression for new features and outcomes, added random seeding

--- baseline_linear_regression.py
+++ baseline_linear_regression.py
@@ -1,4 +1,5 @@
 # code structure inspired by https://www.geeksforgeeks.org/linear-regression-using-pytorch/ 
+import random
 import numpy as np
 import pandas as pd
 import torch
@@ -8,51 +9,93 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# random seeding
+seed = 21
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 
 df = pd.read_excel("merged_city_year_panel milestone updated.xlsx")
-features = [
+
+# this list contains all possible columns we might use as features or outcomes
+# we need to clean this data and remove NAN values 
+to_clean = [
+    "Liability Ratio(%)",
+    "Debt Ratio(%)",
     "GDP(CNY,B)",
     "Growth Rate of GDP(%)",
     "Comprehensive Financial Resources(CNY,B)",
     "Fiscal Self-sufficiency(%)",
     "Budget Revenue(CNY,B)",
-    "Revenue of Government-Managed Funds(CNY,B)"
+    "Revenue of Government-Managed Funds(CNY,B)",
+    "State-owned Land Transfer Income/Budget Revenue(%)",
+    "Real Estate Investment(CNY,B)",
+    "LGFV Interest-bearing Debt(CNY,B)",
+    "Balance of Urban Investment Bond(CNY,B)"
 ]
 
-outcome = "LGFV Interest-bearing Debt(CNY,B) / GDP(CNY,B)"
+for col in to_clean:   # remove all -- cells
+    df = df[df[col] != "--"]
 
-for feature in features:
-    df = df[df[feature] != "--"]
+# convert data type to numbers
+df[to_clean] = df[to_clean].apply(pd.to_numeric, errors='raise')
+df[to_clean] = df[to_clean].apply(pd.to_numeric, errors='raise')
 
-df = df[df[outcome] != "--"]
+# creating new columns by dividing
+df["LGFV Interest-bearing Debt(CNY,B) / GDP(CNY,B)"] = df["LGFV Interest-bearing Debt(CNY,B)"]/df["GDP(CNY,B)"] # outcome 1
+df["Balance of Urban Investment Bond(CNY,B) / GDP(CNY,B)"] = df["Balance of Urban Investment Bond(CNY,B)"]/df["GDP(CNY,B)"] # outcome 2
+df["Real Estate Investment(CNY,B) / GDP(CNY,B)"] = df["Real Estate Investment(CNY,B)"]/df["GDP(CNY,B)"] # potential feature
 
+# create train/test split
 train_data = pd.DataFrame()
 test_data = pd.DataFrame()
-
 for year, group in df.groupby('year'):
-    train, test = train_test_split(group, test_size=.2)
+    train, test = train_test_split(group, test_size=.2, random_state=seed)
     train_data = pd.concat([train_data, train], ignore_index=True)
     test_data = pd.concat([test_data, test], ignore_index=True)
 
-train_data[features] = train_data[features].apply(pd.to_numeric, errors='raise')
-test_data[features] = test_data[features].apply(pd.to_numeric, errors='raise')
-train_data[outcome] = train_data[outcome].apply(pd.to_numeric, errors='raise')
-test_data[outcome] = test_data[outcome].apply(pd.to_numeric, errors='raise')
+all_features = [
+    "Liability Ratio(%)",
+    "Debt Ratio(%)",
+    "Growth Rate of GDP(%)",
+    "Comprehensive Financial Resources(CNY,B)",
+    "Fiscal Self-sufficiency(%)",
+    "Budget Revenue(CNY,B)",
+    "Revenue of Government-Managed Funds(CNY,B)",
+    "State-owned Land Transfer Income/Budget Revenue(%)",
+    "Real Estate Investment(CNY,B)"
+] # list of all possible features
 
+features1 = [
+    "Liability Ratio(%)",
+    "Debt Ratio(%)",
+    "Growth Rate of GDP(%)",
+    "Comprehensive Financial Resources(CNY,B)",
+    "Fiscal Self-sufficiency(%)",
+    "Budget Revenue(CNY,B)",
+    "Revenue of Government-Managed Funds(CNY,B)",
+    "State-owned Land Transfer Income/Budget Revenue(%)"
+] # without real estate investment
+
+features2 = features1 + ["Real Estate Investment(CNY,B) / GDP(CNY,B)"] # with real estate investment
+
+outcome1 = "LGFV Interest-bearing Debt(CNY,B) / GDP(CNY,B)" 
+outcome2 = "Balance of Urban Investment Bond(CNY,B) / GDP(CNY,B)"
+
+feature_combinations = [features1, features2] # list of lists of feature combinations
+outcomes = [outcome1, outcome2] # list of possible outcomes
+                                                                
+
+# scale data
 scaler = StandardScaler()
-scaler.fit(train_data[features])
-train_data[features] = scaler.transform(train_data[features])
-scaler.fit(test_data[features])
-test_data[features] = scaler.transform(test_data[features])
-scaler.fit(train_data[outcome].values.reshape(-1, 1))
-train_data[outcome] = scaler.transform(train_data[outcome].values.reshape(-1, 1))
-scaler.fit(test_data[outcome].values.reshape(-1, 1))
-test_data[outcome] = scaler.transform(test_data[outcome].values.reshape(-1, 1))
-
-x_train = torch.from_numpy(train_data[features].to_numpy()).type(torch.float32)
-x_test = torch.from_numpy(test_data[features].to_numpy()).type(torch.float32)
-y_train = torch.from_numpy(train_data[outcome].to_numpy()).type(torch.float32)
-y_test = torch.from_numpy(test_data[outcome].to_numpy()).type(torch.float32)
+scaler.fit(train_data[all_features])
+train_data[all_features] = scaler.transform(train_data[all_features])
+scaler.fit(test_data[all_features])
+test_data[all_features] = scaler.transform(test_data[all_features])
+scaler.fit(train_data[outcomes])
+train_data[outcomes] = scaler.transform(train_data[outcomes])
+scaler.fit(test_data[outcomes])
+test_data[outcomes] = scaler.transform(test_data[outcomes])
 
 class LinearRegression(nn.Module):
     def __init__(self, num_features):
@@ -61,27 +104,36 @@ class LinearRegression(nn.Module):
 
     def forward(self, x):
         return self.linear(x)
-    
-model = LinearRegression(6)
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-num_epochs = 1000
-for i in range(num_epochs):
-    optimizer.zero_grad()
-    y_predict = model(x_train)
-    loss = criterion(y_predict.squeeze(), y_train)
-    loss.backward()
-    optimizer.step()
-    if i%100 == 0:
-        print(f'epoch: {i}, loss: {loss}')
+for features in feature_combinations:
+    for outcome in outcomes:
+        x_train = torch.from_numpy(train_data[features].to_numpy()).type(torch.float32)
+        x_test = torch.from_numpy(test_data[features].to_numpy()).type(torch.float32)
+        y_train = torch.from_numpy(train_data[outcome].to_numpy()).type(torch.float32)
+        y_test = torch.from_numpy(test_data[outcome].to_numpy()).type(torch.float32)
 
-predictions = model(x_test).squeeze()
+        model = LinearRegression(len(features))
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-print(f'Test Set MSE: {criterion(predictions.squeeze(), y_test)}')
+        num_epochs = 1000
+        for i in range(num_epochs):
+            optimizer.zero_grad()
+            y_predict = model(x_train)
+            loss = criterion(y_predict.squeeze(), y_train)
+            loss.backward()
+            optimizer.step()
+            if i%100 == 0:
+                print(f'epoch: {i}, loss: {loss}')
 
-plt.clf()
-plt.scatter(x_test[:, 0].numpy(), predictions.detach().numpy(), label='Predictions', alpha=0.4, color='blue') # pick one item on x_axis
-plt.scatter(x_test[:, 0].numpy(), y_test.detach().numpy(), label='True Values', alpha = 0.4, color='orange') # pick one item on x_axis
-plt.legend()
-plt.savefig("linear_regression_scatterplot.png")
+        predictions = model(x_test).squeeze()
+
+        print(f'Features: {features}')
+        print(f'Outcome: {outcome}')
+        print(f'Test Set MSE: {criterion(predictions.squeeze(), y_test)}')
+
+        plt.clf()
+        plt.scatter(x_test[:, 0].numpy(), predictions.detach().numpy(), label='Predictions', alpha=0.4, color='blue') # pick one item on x_axis
+        plt.scatter(x_test[:, 0].numpy(), y_test.detach().numpy(), label='True Values', alpha = 0.4, color='orange') # pick one item on x_axis
+        plt.legend()
+        plt.savefig("linear_regression_scatterplot.png")
